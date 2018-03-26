1450300fc378604a1820c634f3f8c05c08579741
Philip Helger <philip@helger.com>
2018-03-26 03:50:07 +0200

    Added forbidden APIs check

--- ph-masterdata/src/test/java/com/helger/masterdata/tools/MainReadISO639_2CodeList.java
+++ ph-masterdata/src/test/java/com/helger/masterdata/tools/MainReadISO639_2CodeList.java
@@ -16,13 +16,13 @@
  */
 package com.helger.masterdata.tools;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.commons.io.stream.NonBlockingBufferedReader;
 import com.helger.commons.string.StringHelper;
 import com.helger.xml.microdom.IMicroDocument;
 import com.helger.xml.microdom.IMicroElement;
@@ -39,47 +39,50 @@ public class MainReadISO639_2CodeList
   public static void main (final String [] args) throws IOException
   {
     final String sRevision = "20130111";
-    final BufferedReader aReader = new BufferedReader (new ClassPathResource ("ISO-639-2_utf-8.txt").getReader (StandardCharsets.UTF_8));
+
     final IMicroDocument aDoc = new MicroDocument ();
     final IMicroElement eRoot = aDoc.appendElement ("iso639-2");
-    String sLine;
-    // Skip the BOM!
-    aReader.read ();
-    while ((sLine = aReader.readLine ()) != null)
+    try (final NonBlockingBufferedReader aReader = new NonBlockingBufferedReader (new ClassPathResource ("ISO-639-2_utf-8.txt").getReader (StandardCharsets.UTF_8)))
     {
-      // An alpha-3 (bibliographic) code
-      // an alpha-3 (terminologic) code (when given)
-      // an alpha-2 code (when given)
-      // an English name
-      // a French name
-      final String [] aParts = StringHelper.getExplodedArray ('|', sLine);
-      if (aParts.length != 5)
-        throw new IllegalStateException ();
-      final String sAlpha3B = aParts[0];
-      final String sAlpha3T = aParts[1];
-      final String sAlpha2 = aParts[2];
-      final String sEN = aParts[3];
-      final String sFR = aParts[4];
+      String sLine;
+      // Skip the BOM!
+      aReader.read ();
+      while ((sLine = aReader.readLine ()) != null)
+      {
+        // An alpha-3 (bibliographic) code
+        // an alpha-3 (terminologic) code (when given)
+        // an alpha-2 code (when given)
+        // an English name
+        // a French name
+        final String [] aParts = StringHelper.getExplodedArray ('|', sLine);
+        if (aParts.length != 5)
+          throw new IllegalStateException ();
+        final String sAlpha3B = aParts[0];
+        final String sAlpha3T = aParts[1];
+        final String sAlpha2 = aParts[2];
+        final String sEN = aParts[3];
+        final String sFR = aParts[4];
 
-      if (StringHelper.hasNoText (sAlpha3B))
-        throw new IllegalArgumentException ("Alpha3B");
-      if (StringHelper.hasNoText (sEN))
-        throw new IllegalArgumentException ("EN");
-      if (StringHelper.hasNoText (sFR))
-        throw new IllegalArgumentException ("FR");
+        if (StringHelper.hasNoText (sAlpha3B))
+          throw new IllegalArgumentException ("Alpha3B");
+        if (StringHelper.hasNoText (sEN))
+          throw new IllegalArgumentException ("EN");
+        if (StringHelper.hasNoText (sFR))
+          throw new IllegalArgumentException ("FR");
 
-      // "Reserved for local use"
-      if (sAlpha3B.equals ("qaa-qtz"))
-        continue;
+        // "Reserved for local use"
+        if (sAlpha3B.equals ("qaa-qtz"))
+          continue;
 
-      final IMicroElement eItem = eRoot.appendElement ("item");
-      eItem.setAttribute ("alpha3", sAlpha3B.toLowerCase (Locale.US));
-      if (StringHelper.hasText (sAlpha3T))
-        eItem.setAttribute ("alpha3t", sAlpha3T.toLowerCase (Locale.US));
-      if (StringHelper.hasText (sAlpha2))
-        eItem.setAttribute ("alpha2", sAlpha2.toLowerCase (Locale.US));
-      eItem.setAttribute ("en", sEN);
-      eItem.setAttribute ("fr", sFR);
+        final IMicroElement eItem = eRoot.appendElement ("item");
+        eItem.setAttribute ("alpha3", sAlpha3B.toLowerCase (Locale.US));
+        if (StringHelper.hasText (sAlpha3T))
+          eItem.setAttribute ("alpha3t", sAlpha3T.toLowerCase (Locale.US));
+        if (StringHelper.hasText (sAlpha2))
+          eItem.setAttribute ("alpha2", sAlpha2.toLowerCase (Locale.US));
+        eItem.setAttribute ("en", sEN);
+        eItem.setAttribute ("fr", sFR);
+      }
     }
     MicroWriter.writeToFile (aDoc, new File ("src/main/resources/codelists/iso639-2-data-" + sRevision + ".xml"));
   }
