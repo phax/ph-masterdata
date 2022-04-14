51f0f52e29d5a15af246a674ff71c139536dd814
Philip Helger <philip@helger.com>
2022-04-14 23:25:11 +0200

    Updated

--- ph-masterdata/src/test/java/com/helger/masterdata/tools/MainReadPostalCodeListExcel.java
+++ ph-masterdata/src/test/java/com/helger/masterdata/tools/MainReadPostalCodeListExcel.java
@@ -17,13 +17,9 @@
 package com.helger.masterdata.tools;
 
 import java.io.File;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -31,12 +27,12 @@ import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,8 +42,11 @@ import com.helger.commons.annotation.ReturnsImmutableObject;
 import com.helger.commons.collection.CollectionHelper;
 import com.helger.commons.collection.impl.CommonsArrayList;
 import com.helger.commons.collection.impl.CommonsHashMap;
+import com.helger.commons.collection.impl.CommonsHashSet;
 import com.helger.commons.collection.impl.ICommonsList;
 import com.helger.commons.collection.impl.ICommonsMap;
+import com.helger.commons.collection.impl.ICommonsSet;
+import com.helger.commons.compare.CompareHelper;
 import com.helger.commons.datetime.PDTFactory;
 import com.helger.commons.io.file.FileHelper;
 import com.helger.commons.string.StringHelper;
@@ -62,7 +61,7 @@ public class MainReadPostalCodeListExcel
 {
   private static final Logger LOGGER = LoggerFactory.getLogger (MainReadPostalCodeListExcel.class);
   private static final String PREFIX_ONE_CODE = "one code: ";
-  private static final String NO_CODES = "- no codes -";
+  private static final ICommonsSet <String> NO_CODES = new CommonsHashSet <> ("- no codes -", "– no codes -");
 
   private static final class Item
   {
@@ -74,7 +73,7 @@ public class MainReadPostalCodeListExcel
     private final String m_sNote;
 
     public Item (@Nonnull @Nonempty final String sCountry,
-                 @Nullable final Date aValidFrom,
+                 @Nullable final LocalDate aValidFrom,
                  @Nonnull @Nonempty final String sISO,
                  @Nonnull @Nonempty final List <String> aFormats,
                  @Nullable final String sNote)
@@ -83,7 +82,7 @@ public class MainReadPostalCodeListExcel
       ValueEnforcer.notEmpty (sISO, "ISO");
       ValueEnforcer.notEmpty (aFormats, "formats");
       m_sCountry = sCountry;
-      m_aValidFrom = aValidFrom == null ? null : PDTFactory.createLocalDate (aValidFrom);
+      m_aValidFrom = aValidFrom;
       m_sISO = sISO;
       m_aFormats = aFormats;
       m_sNote = sNote;
@@ -137,10 +136,10 @@ public class MainReadPostalCodeListExcel
   public static void main (final String [] args) throws Exception
   {
     final String sSource = "http://en.wikipedia.org/wiki/List_of_postal_codes";
-    final String sRevision = "20130209";
+    final String sRevision = "20220414";
 
-    final File f = new File ("src/test/resources/postalcode/" + sRevision + "PostalCodes.xls");
-    try (final Workbook aWB = new HSSFWorkbook (FileHelper.getInputStream (f)))
+    final File f = new File ("src/test/resources/postalcode/" + sRevision + "PostalCodes.xlsx");
+    try (final Workbook aWB = new XSSFWorkbook (FileHelper.getInputStream (f)))
     {
       final Sheet aSheet = aWB.getSheetAt (0);
       final Iterator <Row> it = aSheet.rowIterator ();
@@ -161,37 +160,53 @@ public class MainReadPostalCodeListExcel
       {
         final Row aRow = it.next ();
         ++nRow;
-        final String sCountry = ExcelReadHelper.getCellValueString (aRow.getCell (0));
-        if (StringHelper.hasNoText (sCountry))
+
+        // Country name
+        final String sCountryName = ExcelReadHelper.getCellValueString (aRow.getCell (0));
+        if (StringHelper.hasNoText (sCountryName))
         {
           LOGGER.warn ("Line " + nRow + ": No country name present");
           continue;
         }
+
+        // Validity
         final Cell aDateCell = aRow.getCell (1);
-        Date aIntroducedDate = null;
+        LocalDate aIntroducedDate = null;
         if (aDateCell != null && aDateCell.getCellType () != CellType.BLANK)
         {
           final Number aNum = ExcelReadHelper.getCellValueNumber (aDateCell);
-          final int nYear = aNum.intValue ();
-          if (nYear > 1800 && nYear < 3000)
-            aIntroducedDate = Date.from (Instant.from (PDTFactory.createLocalDate (nYear, Month.JANUARY, 1)
-                                                                 .atStartOfDay ()
-                                                                 .toInstant (ZoneOffset.UTC)));
-          else
-            aIntroducedDate = ExcelReadHelper.getCellValueJavaDate (aDateCell);
+          if (aNum != null)
+          {
+            final int nYear = aNum.intValue ();
+            if (nYear > 1800 && nYear < 3000)
+              aIntroducedDate = PDTFactory.createLocalDate (nYear, Month.JANUARY, 1);
+            else
+              aIntroducedDate = ExcelReadHelper.getCellValueLocalDate (aDateCell);
+          }
         }
-        final String sISO = ExcelReadHelper.getCellValueString (aRow.getCell (2));
-        if (StringHelper.hasNoText (sISO))
+
+        // Country ISO
+        final String sCountryISO = ExcelReadHelper.getCellValueString (aRow.getCell (2));
+        if (StringHelper.hasNoText (sCountryISO))
         {
-          LOGGER.warn ("Line " + nRow + ": No ISO code for " + sCountry);
+          LOGGER.warn ("Line " + nRow + ": No Country ISO code for " + sCountryName);
           continue;
         }
+
+        // Postal code Format
         final String sFormat = ExcelReadHelper.getCellValueString (aRow.getCell (3));
-        if (NO_CODES.equals (sFormat) || StringHelper.hasNoText (sFormat))
+        if (NO_CODES.contains (sFormat) || StringHelper.hasNoText (sFormat))
           continue;
-        final List <String> aFormats = StringHelper.getExploded ("\n", sFormat);
-        final String sNote = ExcelReadHelper.getCellValueString (aRow.getCell (4));
-        aItems.add (new Item (sCountry, aIntroducedDate, sISO, aFormats, sNote));
+        final ICommonsList <String> aFormats = StringHelper.getExploded ("\n",
+                                                                         StringHelper.replaceAll (sFormat, ",", "\n"));
+        for (int i = 0; i < aFormats.size (); ++i)
+          aFormats.set (i, aFormats.get (i).trim ());
+
+        // Skip street name format
+
+        // The note
+        final String sNote = ExcelReadHelper.getCellValueString (aRow.getCell (6));
+        aItems.add (new Item (sCountryName, aIntroducedDate, sCountryISO, aFormats, sNote));
       }
 
       // Convert to map, where the key is the ISO
@@ -202,7 +217,8 @@ public class MainReadPostalCodeListExcel
       // Sort all sub-lists by introduction date
       for (final List <Item> aSubList : aMap.values ())
       {
-        aSubList.sort (Comparator.comparing (Item::getValidFrom));
+        // null-safe sorting
+        aSubList.sort ( (x, y) -> CompareHelper.compare (x.getValidFrom (), y.getValidFrom (), true));
         for (int i = 1; i < aSubList.size (); ++i)
         {
           final Item aPrevItem = aSubList.get (i - 1);
